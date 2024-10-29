853eb025163f94967eda27a2ff35a65917845445
Pablo <pablojimenezc182@gmail.com>
2024-10-29 17:05:06 -0600

    calculadora con vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+ 
+  components: {
+    Calculator
+  }
+}
+</script>
+
